2d1fb1fbb9708038e605ca4c9908f2581418f634
TurboSebo <73588307+TurboSebo@users.noreply.github.com>
2025-11-27 22:35:58 +0100

    initialize project structure with Spring Boot and Maven wrapper

---
+++ src/main/java/com/github/openblogcms/OpenBlogCmsApplication.java
@@ -0,0 +1,13 @@
+package com.github.openblogcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBlogCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBlogCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/openblogcms/OpenBlogCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.openblogcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBlogCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
